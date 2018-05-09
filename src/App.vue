9ae9359f4589f580209c4386331cc50d183928e3
zuiyue2012 <331685622@qq.com>
2018-05-10 01:57:35 +0800

    研判打逃首页
    研判打逃首页

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,56 @@
     <div class="framework-panel">
       <div class="top-menu">
         <div class="log">
-          稽查监控系统
+          <span class="log-img"></span>
+          <span class="log-text">稽查监控系统</span>
+          <span style="font-size: 10px;">  2017 年 12 月 21 日    星期三    12`C   晴</span>
+        </div>
+        <div style="float: right; padding: 20px; cursor: pointer;">
+          <span class="logout"></span>
+          <span style="font-size:15px;">退出</span>
         </div>
       </div>
-      <div class="bottom-content">
-        <div class="left-menu"></div>
-        <div class="right-content"></div>
+      <div class="bottom-content display-flex flow-x">
+        <div class="left-menu">
+          <div style="margin-bottom: 6px;">职位名称</div>
+          <div class="item user-info" style="height: 70px;">
+            <span class="image"></span>
+            <div class="text">姓名</div>
+          </div>
+          <div class="item user-profile">
+            <span class="image"></span>
+            <div class="text">个人概述</div>
+          </div>
+          <div class="item affairs">
+            <span class="image"></span>
+            <div class="text">日常事务</div>
+          </div>
+          <div class="item audit active">
+            <span class="image"></span>
+            <div class="text">研判打逃</div>
+          </div>
+          <div class="item release">
+            <span class="image"></span>
+            <div class="text">任务发布</div>
+          </div>
+          <div class="item work-plan">
+            <span class="image"></span>
+            <div class="text">工作计划</div>
+          </div>
+          <div class="item statistics">
+            <span class="image"></span>
+            <div class="text">统计报表</div>
+          </div>
+          <div class="item system">
+            <span class="image"></span>
+            <div class="text">系统维护</div>
+          </div>
+        </div>
+        <div class="flex-box-1">
+          <router-view/>
+        </div>
       </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -28,21 +69,155 @@
   }
 
   .top-menu {
-    height: 60px;
+    height: 65px;
     width: 100%;
-    background: cornflowerblue;
+    background: #5b79af;
     color: #FFFFFF;
   }
 
+  .top-menu .log {
+    float: left;
+    padding: 18px 30px 0;
+  }
+
+  .log .log-img {
+    background: url("./assets/image/log.png") no-repeat;
+    margin-top: 2px;
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    margin-right: 12px;
+    background-size: 100%;
+    vertical-align: top;
+  }
+
+  .log .log-text {
+    font-size: 20px;
+    margin-right: 10px;
+  }
+
+  .logout {
+    background: url("./assets/image/menu-logout.png") no-repeat;
+    margin-top: 4px;
+    display: inline-block;
+    width: 15px;
+    height: 15px;
+    margin-right: 8px;
+    background-size: 100%;
+    vertical-align: top;
+  }
+
   .bottom-content {
-
+    width: 100%;
   }
 
-  .bottom-content .left-menu{
-
+  .bottom-content .left-menu {
+    float: left;
+    width: 120px;
+    background: #262c38;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 14px;
+    padding: 12px 0;
   }
 
-  .bottom-content .right-content{
+  .left-menu .item {
+    width: 100%;
+    height: 60px;
+    text-align: center;
+    padding: 5px 0;
+  }
 
+  .item .image {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    margin-top: 8px;
+  }
+
+  .user-info .image{
+    background: url("./assets/image/menu-photo.png") no-repeat;
+    background-size: 100%;
+    width: 40px;
+    height: 40px;
+    background-color: #fff;
+    border-radius: 50%;
+    background-size: 35px 40px;
+    background-position: 2px 3px;
+  }
+
+  .user-profile .image{
+    background: url("./assets/image/menu-profile.png") no-repeat;
+    background-size: 100%;
+  }
+
+  .affairs .image{
+    background: url("./assets/image/menu-affairs.png") no-repeat;
+    background-size: 100%;
+  }
+
+  .audit .image{
+    background: url("./assets/image/menu-audit.png") no-repeat;
+    background-size: 100%;
+  }
+
+  .release .image{
+    background: url("./assets/image/menu-release.png") no-repeat;
+    background-size: 100%;
+  }
+
+  .work-plan .image{
+    background: url("./assets/image/menu-work-plan.png") no-repeat;
+    background-size: 100%;
+  }
+
+  .statistics .image{
+    background: url("./assets/image/menu-statistics.png") no-repeat;
+    background-size: 100%;
+  }
+
+  .system .image{
+    background: url("./assets/image/menu-system.png") no-repeat;
+    background-size: 100%;
+  }
+
+  .item .text {
+    font-size: 12px;
+    margin-top: 2px;
+  }
+
+  .left-menu .item.active {
+    background: #2b3244;
+    color: #6388bb;
+  }
+
+  .bottom-content .right-content {
+    float: right;
+    width: calc(100% - 120px);
+  }
+
+  .display-flex {
+    display: -webkit-flex;
+    display: flex;
+  }
+
+  .flow-x {
+    flex-flow: row;
+  }
+
+  .flow-y {
+    flex-flow: column;
+  }
+
+  .flex-box-1 {
+    flex: 1;
+  }
+
+  .flex-box-2 {
+    flex: 2;
+  }
+
+  .flex-box-3 {
+    flex: 3;
   }
 </style>
---
+++ src/components/Judgment.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="display-flex flow-y" style="height: 100%;">
+    <div class="bread-crumbs">
+      <span>当前位置：</span>
+      <span>研判打逃</span>
+    </div>
+    <div class="content-box flex-box-1">
+      <div class="item-list-box">
+        <div class="item-box-title">
+          逃费类型
+        </div>
+        <div class="display-flex flow-x" style="padding: 20px 30px;flex-wrap: wrap;">
+          <div class="item total">
+            <span class="item-log"></span>
+            <div class="item-text">全计重逃费</div>
+          </div>
+          <div class="item peer">
+            <span class="item-log"></span>
+            <div class="item-text">非正常同行逃费</div>
+          </div>
+          <div class="item counterfeit">
+            <span class="item-log"></span>
+            <div class="item-text">假冒优惠车逃费</div>
+          </div>
+          <div class="item critical">
+            <span class="item-log"></span>
+            <div class="item-text">临界车逃费</div>
+          </div>
+          <div class="item etc-mtc">
+            <span class="item-log"></span>
+            <div class="item-text">ETC&MTC混合逃费</div>
+          </div>
+          <div class="item green">
+            <span class="item-log"></span>
+            <div class="item-text">假冒绿通行逃费</div>
+          </div>
+        </div>
+      </div>
+      <div class="item-list-box">
+        <div class="item-box-title">
+          智能稽查
+        </div>
+        <div class="display-flex flow-x" style="padding: 20px 30px;flex-wrap: wrap;">
+          <div class="item model">
+            <span class="item-log"></span>
+            <div class="item-text">单车模型分析</div>
+          </div>
+          <div class="item blacklist">
+            <span class="item-log"></span>
+            <div class="item-text">黑名单</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Judgment',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .bread-crumbs {
+    padding: 10px 15px;
+    font-size: 12px;
+    box-shadow: 2px 2px 5px #e6e6e6;
+    z-index: 9;
+  }
+
+  .content-box {
+    background-color: #f7faff;
+    padding: 20px 35px;
+  }
+
+  .item-list-box {
+
+  }
+
+  .item-list-box .item-box-title {
+    border-left: 5px solid #5a78af;
+    padding-left: 20px;
+  }
+
+  .item-list-box .item {
+    width: 280px;
+    height: 120px;
+    box-shadow: 0px 0px 10px #e6e6e6;
+    margin: 0 28px 20px;
+    background-color: #FFFFFF;
+    border-radius: 3px;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .item .item-log {
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    margin-top: 25px;
+  }
+
+  .item .item-text {
+    font-size: 12px;
+  }
+
+  .blacklist .item-log {
+    background: url("../assets/image/item-blacklist.png") no-repeat;
+    background-size: 100%;
+  }
+  .counterfeit .item-log {
+    background: url("../assets/image/item-counterfeit.png") no-repeat;
+    background-size: 100%;
+  }
+  .critical .item-log {
+    background: url("../assets/image/item-critical.png") no-repeat;
+    background-size: 100%;
+  }
+  .etc-mtc .item-log {
+    background: url("../assets/image/item-etc-mtc.png") no-repeat;
+    background-size: 100%;
+  }
+  .green .item-log {
+    background: url("../assets/image/item-green.png") no-repeat;
+    background-size: 100%;
+  }
+  .model .item-log {
+    background: url("../assets/image/item-model.png") no-repeat;
+    background-size: 100%;
+  }
+  .peer .item-log {
+    background: url("../assets/image/item-peer.png") no-repeat;
+    background-size: 100%;
+  }
+  .total .item-log {
+    background: url("../assets/image/item-total.png") no-repeat;
+    background-size: 100%;
+  }
+</style>
